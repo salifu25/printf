de676d75097291dff6af382cc5b6cfc5505fdd61
ADENIYI <111161031+ADETOLAR01@users.noreply.github.com>
2022-10-18 04:19:46 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,86 +1,27 @@
 #include "main.h"
-
 /**
- * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated.
- *
- * Return: 1 if c is printable, 0 otherwise
+ * print_rev - print a string in reverse
+ * @ap:handle %r
+ * @modif :struct modifier containig modifier fields
+ * Return: lenght
  */
-int is_printable(char c)
+char *print_rev(modifier_t *modif, va_list ap)
 {
-	if (c >= 32 && c < 127)
-		return (1);
+	int i;
+	int length;
+	char *str, *ret;
 
-	return (0);
-}
+	if (!ap || !modif)
+		return (0);
+	str = va_arg(ap, char *);
+	length = _strlen(str);
+	ret = malloc(length + 1);
+	if (!ret)
+		return (NULL);
 
-/**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars.
- * @i: Index at which to start appending.
- * @ascii_code: ASSCI CODE.
- * Return: Always 3
- */
-int append_hexa_code(char ascii_code, char buffer[], int i)
-{
-	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format code is always 2 digits long */
-	if (ascii_code < 0)
-		ascii_code *= -1;
+	for (i = 0; i < length; i++)
+		ret[i] = str[length - i - 1];
+	ret[i] = '\0';
 
-	buffer[i++] = '\\';
-	buffer[i++] = 'x';
-
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
-
-	return (3);
-}
-
-/**
- * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
- *
- * Return: 1 if c is a digit, 0 otherwise
- */
-int is_digit(char c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-
-	return (0);
-}
-
-/**
- * convert_size_number - Casts a number to the specified size
- * @num: Number to be casted.
- * @size: Number indicating the type to be casted.
- *
- * Return: Casted value of num
- */
-long int convert_size_number(long int num, int size)
-{
-	if (size == S_LONG)
-		return (num);
-	else if (size == S_SHORT)
-		return ((short)num);
-
-	return ((int)num);
-}
-
-/**
- * convert_size_unsgnd - Casts a number to the specified size
- * @num: Number to be casted
- * @size: Number indicating the type to be casted
- *
- * Return: Casted value of num
- */
-long int convert_size_unsgnd(unsigned long int num, int size)
-{
-	if (size == S_LONG)
-		return (num);
-	else if (size == S_SHORT)
-		return ((unsigned short)num);
-
-	return ((unsigned int)num);
+	return (ret);
 }
